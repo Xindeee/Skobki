ebf0672087daac9b4af54aad78c634b3a8d7932b
Xindeee <76115520+Xindeee@users.noreply.github.com>
2021-09-14 22:15:03 +0300

    14.09.2021

---
+++ Skobki.cpp
@@ -0,0 +1,39 @@
+﻿#include <stack>
+#include <string>
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str;
+    cin >> str;
+    stack<char> steck;
+    int i;
+    for (i = 0; i < str.length(); i++) {
+        if (str[i] == '(' || str[i] == '{' || str[i] == '[') {
+            steck.push(str[i]);
+            continue;
+        }
+        else if (steck.empty()) {
+            steck.push(str[i]);
+            break;
+        }
+        else if (str[i] == ')' && (steck.top() == '[' || steck.top() == '{')) {
+            break;
+        }
+        else if (str[i] == '}' && (steck.top() == '(' || steck.top() == '[')) {
+            break;
+        }
+        else if (str[i] == ']' && (steck.top() == '{' || steck.top() == '(')) {
+            break;
+        }
+        steck.pop();
+    }
+    if (steck.empty()) {
+        cout << "balanced" << endl;
+    }
+    else {
+        cout << "not balanced" << endl;
+        cout << i << endl;
+    }
+    return 0;
+}
